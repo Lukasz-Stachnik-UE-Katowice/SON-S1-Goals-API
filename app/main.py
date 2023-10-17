9cecfc985ff254ce2e382116523fa333d8f1b024
MichalZx <115743546+MichalZx@users.noreply.github.com>
2023-10-17 12:53:05 +0200

    Delete request

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from requests import Session
+
 from .routers import goals
 
 app = FastAPI()
@@ -8,3 +10,19 @@ app.include_router(goals.router)
 @app.get("/")
 async def root():
     return {"message": "Hello Students!"}
+
+@app.delete("/goals/{goal_id}")
+async def delete_goal(goal_id: int):
+    global goals
+    goal_to_delete = None
+
+    for goal in goals:
+        if goal["goal_id"] == goal_id:
+            goals.remove(goal_id)
+            break
+
+    if goal_to_delete is None:
+        raise HTTPException(status_code=404, detail="Cel o podanym goal_id nie istnieje")
+
+    goals.remove(goal_to_delete)
+    return {"message": "Cel został usunięty"}
