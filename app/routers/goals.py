98421269e898f761dc410c35d108d6b45d7411d6
MichalZx <115743546+MichalZx@users.noreply.github.com>
2023-10-19 17:21:22 +0200

    First commit

--- app/routers/goals.py
+++ app/routers/goals.py
@@ -48,6 +48,10 @@ async def post_progress_goal(progress: float):
     return 
 
 @router.post("/goals/{goal_id}/archive", tags=["goals"])
-async def archive_goal(goal_id: str): 
-    # Here we want to archive the goal 
+async def archive_goal(goal_id: str):
+    for goal in goalTestList:
+        if goal.id == goal_id:
+            goalTestList.remove(goal)
+            return {"message": "Targeted goal destroyed"}
+    return HTTPException(status_code=404, detail="Targeted goal does not exist")
     return 
