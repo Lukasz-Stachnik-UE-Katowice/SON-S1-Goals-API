e3eec7f43eab43b24a00d7c447de17ddb9ee7bae
KSSidll <kolasasidll@gmail.com>
2023-11-07 11:46:25 +0100

    fix???

--- app/routers/goals.py
+++ app/routers/goals.py
@@ -1,6 +1,7 @@
 from uuid import UUID
 from fastapi import APIRouter
 from .models.goal import Goal
+import requests
 
 goalTestList = [
     Goal(id= '1', progress= 0.60),
@@ -14,55 +15,25 @@ data = {
 }
 router = APIRouter()
 
-@router.get("/goals", tags=["goals"])
-async def get_goals():
-    # Here we want to return all goals in the database
-    return []
-
 @router.get("/goals/{goal_id}", tags=["goals"])
 async def get_goal(goals_id: UUID):
     # Here we want to return goal that user whant's to see the details of
     return []
-
-@router.get("/goals/{username}", tags=["goals"])
-async def get_user_goals(username: str): 
-    # Here we want to return all goals in the database for given user
-    return data.get(username, [])
 
 @router.post("/goals", tags=["goals"])
 async def post_goal(goal): #Here we need to add goal model that is going to be created https://fastapi.tiangolo.com/tutorial/body/
     # Here we want to add new goal to the database and return it back with status
     return data
 
-@router.put("/goals/{goal_id}", tags=["goals"])
-async def update_goal(goal_id: UUID, goal): 
-    # Here we want to update goal in the database and return only status
-    return 
-
-@router.delete("/goals/{goal_id}", tags=["goals"])
-async def delete_goal(goal_id: UUID): 
-    # Here we want to delete goal from the database, and return status
-    return 
-
 @router.get("/goals", tags=["goals"])
 async def get_goals():
     # Here we want to return all goals in the database
-    return []
-
-@router.get("/goals/{goal_id}", tags=["goals"])
-async def get_goal(goals_id: UUID):
-    # Here we want to return goal that user whant's to see the details of
     return []
 
 @router.get("/goals/{username}", tags=["goals"])
 async def get_user_goals(username: str):
     # Here we want to return all goals in the database for given user
     return [{"goal": "Learn Python"}]
-
-@router.post("/goals", tags=["goals"])
-async def post_goal(goal): #Here we need to add goal model that is going to be created https://fastapi.tiangolo.com/tutorial/body/
-    # Here we want to add new goal to the database and return it back with status
-    return ""
 
 @router.put("/goals/{goal_id}", tags=["goals"])
 async def update_goal(goal_id: str, goal_obj: Goal):
